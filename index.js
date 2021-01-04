566faa5fb7506828d29ecc1c4a5d102283de0c73
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2021-01-04 18:20:53 +0100

    exercise 3.10 phonebook backend step 10

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ app.patch('/api/persons/:id', (req, rsp) => {
     return rsp.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-})
+});
