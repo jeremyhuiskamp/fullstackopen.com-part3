7f5e2abbd6f539f7dc2c8c34c3ca6c575b24ff4a
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2021-01-04 16:09:57 +0100

    exercise 3.2 phonebook backend step 2

--- index.js
+++ index.js
@@ -29,6 +29,12 @@ app.get('/api/persons', (req, rsp) => {
     rsp.json(persons);
 });
 
+app.get('/info', (req, rsp) => {
+    rsp.send(
+        `<p>Phonebook has info for ${persons.length} people</p>` +
+        `<p>${new Date()}</p>`)
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
